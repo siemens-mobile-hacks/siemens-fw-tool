82bcc28177845c75eca8ff52af970f0c04006c99
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2024-08-28 22:00:37 +0000

    info cmd

--- bin/siemens-fw-tool.js
+++ bin/siemens-fw-tool.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 import { program } from "commander";
-import { unpackExe, xbiToFlash } from "../src/index.js";
+import { unpackExe, xbiToFlash, info } from "../src/index.js";
 
 (async () => {
 	program
 		.description('CLI tool for Siemens FW files.');
+
+	program.command('info')
+		.description('Into about fw files.')
+		.argument('<input>', 'path to WinSwup or .exe files')
+		.action(async function (input, outputDir) {
+			await info({input, ...this.optsWithGlobals()});
+		});
 
 	program.command('unpack-exe')
 		.description('Unpack FFSInit or service/update .exe')
@@ -15,8 +22,8 @@ import { unpackExe, xbiToFlash } from "../src/index.js";
 		});
 
 	program.command('fw2bin')
-		.description('Convert .xbz/.xbi/.xfs/.xbb/.exci/.exbi files to fullflash.bin')
-		.argument('<input>', 'path to .xbz')
+		.description('Convert WinSwup files to fullflash.bin')
+		.argument('<input>', 'path to .xbi')
 		.argument('[output-file]', 'output file')
 		.action(async function (input, outputFile) {
 			await xbiToFlash({input, outputFile, ...this.optsWithGlobals()});
--- src/index.js
+++ src/index.js
@@ -1,29 +1,133 @@
 import path from 'node:path';
 import fs from 'node:fs';
-import { convertXbiToFlash, extractFromExe, isXbi, parseXbi } from "@sie-js/fw";
+import { convertXbiToFlash, detectExeType, extractFromExe, getVersionFromFFS, getXbiExtension, isXbi, parseXbi } from "@sie-js/fw";
 import { sprintf } from 'sprintf-js';
 import JSZip from 'jszip';
+import { table as asciiTable, getBorderCharacters } from 'table';
+import chalk from 'chalk';
 
-export async function unpackExe(argv) {
-	let extractedFiles = extractFromExe(fs.readFileSync(argv.input));
+const tableConfig = {
+	singleLine: true,
+	border: getBorderCharacters('void')
+};
 
-	let outDir = argv.outputDir ?? ".";
-	if (!fs.existsSync(outDir))
-		fs.mkdirSync(outDir, { recursive: true });
+export async function info(argv) {
+	let buffer = fs.readFileSync(argv.input);
+	if (isXbi(buffer)) {
+		return await infoXbi(argv, buffer);
+	} else if (detectExeType(buffer)) {
+		return await infoExe(argv, buffer);
+	} else {
+		console.error(`Unknown file format!`);
+	}
+}
+
+async function infoExe(argv, buffer) {
+	console.log(`Exe type: ${detectExeType(buffer)}`);
+
+	let files = await unpackFilesFromExe(argv.input, buffer);
+	console.log(`Files count: ${files.length}`);
 
 	let index = 0;
-	let usedFiles = {};
+	for (let file of files) {
+		console.log('');
+		let contentType = detectContentType(file.buffer);
+		console.log(chalk.bold(`File #${index}:`), file.name, `(${contentType})`);
+		if (contentType == 'xfs' || contentType == 'xbb' || contentType == 'xbi' || contentType == 'xbz')
+			await infoXbi(argv, file.buffer);
+		index++;
+	}
+}
+
+async function infoXbi(argv, buffer) {
+	let xbi = parseXbi(buffer, true);
+
+	let infoTable = [
+		['Title', 'Value'],
+	];
+
+	let addRow = (k, v) => {
+		infoTable.push([chalk.bold(`${k}:`), v.toString()]);
+	};
+
+	addRow('type', detectContentType(buffer));
+
+	for (let k in xbi) {
+		let v = xbi[k];
+		switch (k) {
+			case "dll":
+				infoTable.push([chalk.bold(k), v.replace(/[\0\1]/g, '')]);
+			break;
+
+			case "writes":
+				// skip
+			break;
+
+			case "mapInfo":
+			{
+				let i = 0;
+				for (let mapInfo of v) {
+					addRow(`${k}[${i}]`, mapInfo.toString('hex'));
+					i++;
+				}
+			}
+			break;
+
+			case "dataFlash":
+			case "eraseRegions":
+			{
+				let i = 0;
+				for (let eraseRegion of v) {
+					addRow(`${k}[${i}]`, sprintf("%08X-%08X", eraseRegion.from, eraseRegion.to));
+					i++;
+				}
+			}
+			break;
+
+			case "swCode":
+				addRow(k, sprintf("%08X: %08X", v.addr, v.value));
+			break;
+
+			default:
+				if (Buffer.isBuffer(v)) {
+					infoTable.push([chalk.bold(k), Buffer.from(v).toString('hex')]);
+				} else if (typeof v == "object") {
+					addRow(k, JSON.stringify(v));
+				} else {
+					addRow(k, v);
+				}
+			break;
+		}
+	}
+	console.log(asciiTable(infoTable, tableConfig).trim());
+}
+
+async function unpackFilesFromExe(originalFileName, buffer) {
+	let extractedFiles = extractFromExe(buffer);
+
+	let index = 0;
+	let files = [];
 	for (let extractedFile of extractedFiles) {
 		let contentType = detectContentType(extractedFile);
 
-		let fileName = `${path.basename(argv.input)}.${contentType}`;
+		let fileName = extractedFiles.length > 0 ?
+			`${path.basename(originalFileName)}[${index}].${contentType}` :
+			`${path.basename(originalFileName)}.${contentType}`;
 		try {
 			if (contentType == 'map') {
-				fileName = extractedFile.toString().match(/<([^>]+)>\s*$/si)?.[1] || fileName;
-			} else if (contentType == 'xfs' || contentType == 'xbb' || contentType == 'xbi') {
-				let xbiInfo = parseXbi(extractedFile);
+				fileName = extractedFile.toString().match(/<([^>]+)>\s*$/si)?.[1].replace(/_2D/g, '-') || fileName;
+			} else if (contentType == 'xfs') {
+				let xfsVersion = getVersionFromFFS(convertXbiToFlash(extractedFile));
+				if (xfsVersion)
+					fileName = xfsVersion + ".xfs";
+			} else if (contentType == 'xbb' || contentType == 'xbi' || contentType == 'xbz') {
+				let xbiInfo = parseXbi(extractedFile, true);
 				let lgpid = +xbiInfo.langpack.replace(/^lg/, '');
-				fileName = sprintf("%s_%02d%02d%02d.%s", xbiInfo.model, xbiInfo.svn, lgpid, xbiInfo.t9, contentType);
+				if (xbiInfo.t9 != null) {
+					fileName = sprintf("%s_%02d%02d%02d.%s", xbiInfo.model, xbiInfo.svn, lgpid, xbiInfo.t9, contentType);
+				} else {
+					fileName = sprintf("%s_%02d%02d.%s", xbiInfo.model, xbiInfo.svn, lgpid, contentType);
+				}
 			} else if (contentType == 'zip') {
 				let zip = await (new JSZip()).loadAsync(extractedFile);
 				let zipFile = zip.file("Config/ccq_vinfo.txt");
@@ -38,18 +142,26 @@ export async function unpackExe(argv) {
 
 		fileName = fileName.replace(/\//g, '_');
 
-		if (usedFiles[fileName]) {
-			let newFileName = fileName.replace(/\.([\w\d]+)$/i, '[' + usedFiles[fileName] + ']$1');
-			usedFiles[fileName]++;
-			fileName = newFileName;
-		} else {
-			usedFiles[fileName] = 1;
-		}
-
-		console.log(`${outDir}/${fileName}`);
-		fs.writeFileSync(`${outDir}/${fileName}`, extractedFile);
+		files.push({
+			name: fileName,
+			buffer: extractedFile
+		});
 
 		index++;
+	}
+
+	return files;
+}
+
+export async function unpackExe(argv) {
+	let outDir = argv.outputDir ?? ".";
+	if (!fs.existsSync(outDir))
+		fs.mkdirSync(outDir, { recursive: true });
+
+	let files = await unpackFilesFromExe(argv.input, fs.readFileSync(argv.input));
+	for (let file of files) {
+		console.log(`${outDir}/${file.name}`);
+		fs.writeFileSync(`${outDir}/${file.name}`, file.buffer);
 	}
 }
 
@@ -63,13 +175,8 @@ export async function xbiToFlash(argv) {
 
 function detectContentType(buffer) {
 	if (isXbi(buffer)) {
-		let xbiInfo = parseXbi(buffer);
-		if (xbiInfo.updateType == 'ExtendedNewSplit') {
-			return 'xfs';
-		} else if (xbiInfo.baseline1 == 'klf_bootcore') {
-			return 'xbb';
-		}
-		return 'xbi';
+		let xbiInfo = parseXbi(buffer, true);
+		return getXbiExtension(xbiInfo);
 	} else if (buffer.subarray(0, 4).equals(Buffer.from("504B0304", "hex"))) {
 		return 'zip';
 	} else if (buffer.subarray(0, 13).equals(Buffer.from("[MapFileInfo]"))) {
